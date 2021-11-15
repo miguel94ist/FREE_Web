4bec67ccbeceeb3619f48e1402a675bbef55b47e
Pavel Kuriscak <pavel.kuriscak@gmail.com>
2021-11-14 22:19:16 -0500

    Fixing urls in templates. Cleanup of users in db.

--- freeweb/urls.py
+++ freeweb/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('free.urls')),
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='administration'),
 ]
