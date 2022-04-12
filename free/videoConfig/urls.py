a223e4ccb2e3e4865b901387b9c713f6c1d89496
joao-n-silva <joao.n.silva@tecnico.ulisboa.pt>
2022-04-12 23:44:15 +0100

    pages for video configuration (only accessible by the superusers - only shgown on the super user menu, and url verify user type) VideoConfig.html contains code to mode the video on the window

---
+++ free/videoConfig/urls.py
@@ -0,0 +1,10 @@
+from  free.videoConfig.views import *
+from django.urls import re_path, path
+urlpatterns = []
+
+urlpatterns += [
+    path('videoConfig/', VideoConfigList.as_view(), name='video-config-list'),
+    path('videoConfig/<int:id>/', VideoConfig.as_view(), name='video-config'),
+    path('videoConfig/<int:id>/assign/', VideoConfigAssignStream.as_view(), name='video-config-assign'),
+    path('videoConfig/<int:id>/remove/', VideoConfigRemoveStream.as_view(), name='video-config-remove'),
+]
---
+++ free/videoConfig/views.py
@@ -0,0 +1,286 @@
+from django.views.generic import TemplateView
+from rest_framework import serializers
+from django.http import HttpResponse
+from django.conf import settings
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.shortcuts import render
+
+from free.models import Apparatus
+import requests
+
+
+janus_server_address = settings.JANUS_SERVER_ADDRESSS
+janus_api_secret = settings.JANUS_API_SECRET
+janus_stream_admin_key = settings.JANUS_STREAM_ADMIN_KEY
+
+
+
+
+
+
+def connect_janus_stream(server_address, apisecret):
+
+    data = {
+        "janus": "create",
+        "transaction": "transaction-x",
+        "apisecret" : str(apisecret)
+    }
+    response = requests.post(f'{server_address}/', json = data)
+
+    json_ret = response.json()
+
+    if(response.status_code != 200 or  json_ret['janus']!='success'):
+        print("Error in connection with JANUS")
+        print(response.json())
+        return (0, 0)
+
+    #get session_id
+    session_id = json_ret['data']['id']
+
+    #connect to streamming plugin
+    data = {
+        "janus" : "attach",
+        "plugin" : "janus.plugin.streaming",
+        "transaction": "transaction-x",
+        "apisecret": str(apisecret)
+    }
+
+    response = requests.post(f'{server_address}/'+str(session_id), json = data)
+    json_ret = response.json()
+    if(response.status_code != 200 or  json_ret['janus']!='success'):
+        print("Error in connection with JANUS Streamming plugin")
+        print(response.json())
+        return (0, 0)
+
+    plugin_id = json_ret['data']['id']
+    return   (plugin_id, session_id)
+
+def list_streams(server_address, apisecret):
+    plugin_id, session_id = connect_janus_stream(server_address, apisecret)
+
+    if session_id ==0 or plugin_id == 0:
+        return -1
+
+    data = {
+        "janus": "message",
+        "body": {
+            "request": "list"
+        },
+        "transaction": "transaction-x",
+        "apisecret": apisecret
+    }
+
+    response = requests.post(f'{server_address}/{session_id}/{plugin_id}', json = data)
+    json_ret = response.json()
+    try:
+        return json_ret['plugindata']['data']['list']
+    except:
+        print("Error retrieving JANUS Streams")
+        return []
+    
+
+
+def stream_info(server_address, apisecret, stream_id):
+    plugin_id, session_id = connect_janus_stream(server_address, apisecret)
+    ret_list = []
+
+    if session_id ==0 or plugin_id == 0:
+        return []
+
+    data = {
+        "janus": "message",
+        "body": {
+            "request": "info",
+            "id": int(stream_id)
+        },
+        "transaction": "transaction-x",
+        "apisecret": apisecret
+    }
+    response = requests.post(f'{server_address}/{session_id}/{plugin_id}', json = data)
+    json_ret = response.json()
+    try: 
+        return json_ret['plugindata']['data']['info']
+    except:
+        print(f"Error retrieving JANUS Stream {stream_id} info")
+        return {}
+
+
+
+
+def create_stream(server_address, apisecret, stream_admin_key, name, description):
+    plugin_id, session_id = connect_janus_stream(server_address, apisecret)
+
+    if session_id ==0 or plugin_id == 0:
+        return {}
+
+    data = {
+        "janus": "message",
+        "body": {
+            
+                "request" : "create",
+                "type" : "rtp",
+                "name" : name,
+                "description" : description,
+                "audio" : False,
+                "video" : True,
+                #"data" : <true|false, whether the mountpoint will have datachannels; false by default>,
+                "permanent" : True,
+                "videoport" : 0,  #Porto onde são recebidos os pacotes do stream (ver comando de gstreamer) 
+                "videopt" : 126,  #nao alterar #96
+                "videortpmap" : "H264/90000",  #nao alterar 
+                "videofmtp" : "profile-level-id=42e01f;packetization-mode=1", #nao alterar 
+                "admin_key": stream_admin_key
+        },
+        "transaction": "transaction-x",
+        "apisecret": apisecret,
+    }
+
+    response = requests.post(f'{server_address}/{session_id}/{plugin_id}', json = data)
+    json_ret = response.json()
+    if(json_ret['janus']!= 'success'):
+        print(f"Error creating JANUS Stream")
+        return {}
+    json_plugin = json_ret['plugindata']['data']
+    try: 
+        return json_plugin['stream'] 
+    except:
+        print(f"Error creating JANUS Stream")
+        return {}
+ 
+
+def destroy_stream(server_address, apisecret, stream_admin_key, stream_id):
+    plugin_id, session_id = connect_janus_stream(server_address, apisecret)
+
+    if session_id ==0 or plugin_id == 0:
+        return {}
+
+    data = {
+        "janus": "message",
+        "body": {
+
+                "request" : "destroy",
+                "id": int(stream_id),
+                "permanent" : True,
+                "admin_key": stream_admin_key
+            
+        },
+        "transaction": "transaction-x",
+        "apisecret": apisecret,
+    }
+
+    response = requests.post(f'{server_address}/{session_id}/{plugin_id}', json = data)
+    json_ret = response.json()
+    if(json_ret['janus']!= 'success'):
+        print(f"Error destroying JANUS Streams {stream_id}")
+        return {}
+    json_plugin = json_ret['plugindata']['data']
+    if(json_plugin['streaming'] != 'destroyed'):
+        print(f"Error destroying JANUS Streams {stream_id}")
+        return {}
+    else:
+        return json_ret['plugindata']['data']
+ 
+
+
+class ApparatusSerializer(serializers.ModelSerializer):
+    apparatus_type = serializers.SlugRelatedField(
+        many=False, 
+        read_only=True,
+        slug_field="name"
+    )
+    class Meta:
+        model = Apparatus
+        fields = ['id', 'description', 'video_config','location', 'apparatus_type']
+
+
+
+class VideoConfigList(PermissionRequiredMixin, TemplateView):
+    template_name='videoConfig_list.html'
+    permission_required = 'user.is_supersuser'
+    def get_context_data(self, **kwargs):          
+        context = super().get_context_data(**kwargs)                     
+        context["aparatus_list"] = []
+        
+        for a in set(Apparatus.objects.all()):
+            context["aparatus_list"].append(ApparatusSerializer(a).data)
+        context['janus_info'] = list_streams(janus_server_address, janus_api_secret)
+        return context
+
+
+class VideoConfig(PermissionRequiredMixin,TemplateView):
+    template_name='videoConfig.html'
+    permission_required = 'user.is_supersuser'
+    def get_context_data(self, **kwargs):          
+        context = super().get_context_data(**kwargs)                     
+       
+        ap = Apparatus.objects.filter(id = 1).first()
+        context["ap"] = ApparatusSerializer(ap).data
+        try:
+            si = stream_info(janus_server_address, janus_api_secret, ap.video_config['stream_id'])
+            context['stream_info'] = si
+            context['stream_address'] = janus_server_address.split('/')[2].split(':')[0]
+            context['stream_port'] = si['media'][0]['port']
+        except:
+            context['stream_info'] = {}
+        return context
+
+class VideoConfigAssignStream(VideoConfig):
+    def get(self, request, *args, **kwargs):          
+        context = super().get_context_data(**kwargs)                     
+       
+        ap = Apparatus.objects.filter(id = 1).first()
+        si = {}
+        try:
+            si = stream_info(janus_server_address, janus_api_secret, ap.video_config['stream_id'])
+        except:
+            pass
+        if si != {}:
+            context["ap"] = ApparatusSerializer(ap).data
+            context['stream_info'] = si
+        else:
+            new_stream = create_stream(janus_server_address, 
+                        janus_api_secret,
+                        janus_stream_admin_key, 
+                        f'ap_{ap.id}_{ap.apparatus_type}',
+                        ap.description, 
+            )
+            if(new_stream != {}):
+                ap.video_config = {'stream_id': new_stream['id'], 'stream_server': janus_server_address}
+                ap.save()
+                context['stream_info'] = stream_info(janus_server_address, janus_api_secret, ap.video_config['stream_id'])
+
+            context["ap"] = ApparatusSerializer(ap).data
+            context['stream_address'] = janus_server_address.split('/')[2].split(':')[0]
+            context['stream_port'] = context['stream_info']['media'][0]['port']
+
+        return render(request, 'videoConfig.html', context=context)
+
+class VideoConfigRemoveStream(VideoConfig):
+    def get(self, request, *args, **kwargs):            
+        context = super().get_context_data(**kwargs)                     
+
+        ap = Apparatus.objects.filter(id = 1).first()
+        si = {}
+        try:
+            si = stream_info(janus_server_address, janus_api_secret, ap.video_config['stream_id'])
+        except:
+            pass
+        if si == {}:
+            context["ap"] = ApparatusSerializer(ap).data
+            context['stream_info'] = si
+            return render(request, 'videoConfig.html', context=context)
+        else:
+            destroyed_stream = destroy_stream(janus_server_address, 
+                        janus_api_secret,
+                        janus_stream_admin_key, 
+                        ap.video_config['stream_id'],
+            )
+            ap.video_config = {} 
+            ap.save()
+            context["ap"] = ApparatusSerializer(ap).data
+            context['stream_info'] = {}
+            context['stream_address'] = ""
+            context['stream_port'] = ""
+            return render(request, 'videoConfig.html', context=context)
+
+
