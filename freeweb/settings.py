bd7a058086eb0ad2576fdd632941e5495948954e
Pavel Kuriscak <pavel.kuriscak@gmail.com>
2022-04-21 17:47:08 +0200

    Change settings.py to include proxy settings.

--- freeweb/settings.py
+++ freeweb/settings.py
@@ -101,6 +101,10 @@ DATABASES = {
     }
 }
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+USE_X_FORWARDED_HOST = True
+USE_X_FORWARDED_PORT = True
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
