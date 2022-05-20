633c1fba3d4bea4a200c8fc5286e177827a3e199
Pavel Kuriscak <pavel.kuriscak@gmail.com>
2022-05-20 18:33:35 +0200

    Nicer display of online/offline status.

--- free/views/pages.py
+++ free/views/pages.py
@@ -71,7 +71,7 @@ class ExecutionsFinishedListView(ExecutionsListView):
 class ApparatusTable(Table):
 
     protocols = TemplateColumn(template_name='free/protocols.html')
-    current_status = Column(verbose_name=_('Current status'))
+    current_status = TemplateColumn(verbose_name=_('Current status'), template_name='free/current_status.html')
 
     class Meta:
         model = Apparatus
